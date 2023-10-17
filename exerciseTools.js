e275e8a99d105da5bdcc705826adfe98bc55667e
Jeroen van den Berg <j.vanden.berg1994@gmail.com>
2023-10-17 14:59:42 +0200

    all 1st order question types are up and running

--- exerciseTools.js
+++ exerciseTools.js
@@ -2,15 +2,14 @@ function getRandomExercise(seed) {
     //Switch number to a page readout once that's a button
     //Switch type to "Random" once back-end supports that
     var number = 10;
-    var questionType = "Random";
     if (seed == undefined) {
         seed = Date.now();
     }
-    fetchExercise(questionType, number, seed);
+    fetchExercise(number, seed);
 }
 
-async function fetchExercise(questionType, number, seed) {
-    var response = await fetch(url + "generateExercise/" + questionType + "/" + number + "/" + seed);
+async function fetchExercise(number, seed) {
+    var response = await fetch(url + "generateRandomExercise/" + number + "/" + seed);
     var exercise = await response.json();
     displayExercise(exercise);   
 }  
@@ -20,11 +19,12 @@ function displayExercise(exercise) {
     var exerciseHTML = "";
     var answersHTML = "";
     for (var i = 0; i < numberOfQuestions; i++) {
-        exerciseHTML += "<p>" + (i+1) + ") " + exercise.questionsList[i].questionText + "</p>";
+        exerciseHTML += "<p>" + (i+1) + ") " + exercise.questionsList[i].questionText.replaceAll(".",",") + ".</p>";
         answersHTML += "<p>" + (i+1) + ") " + exercise.questionsList[i].answerKeyString + "</p>";
     }
 
     exerciseHTML += "<br><p><span class='code-text'> code: " + exercise.seed + "</span> </p>";
+    answersHTML = answersHTML.replaceAll(".", ",");
     
     $("#exercisePlaceholder").html(exerciseHTML);
     $("#answersPlaceholder").html(answersHTML);
