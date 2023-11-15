6c1d9ab8e4fd75ce7d1f23281ce0223a169c7f30
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-11-15 15:02:50 +0100

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //Comment out the local or online url
-//var url = "http://localhost:8080/";
-var url = "https://chemcalcbackend.azurewebsites.net/";
+var url = "http://localhost:8080/";
+// var url = "https://chemcalcbackend.azurewebsites.net/";
 
 var emailAddress = "info@molrekenaar.nl";
